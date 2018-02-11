5bafc756f81d09bc904503138f2322c060a63c4a
Mosuswalks <mosuswalks@gmail.com>
2018-02-10 20:25:49 -0500

    Modified assetsPublicPath

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/mosuswalks.github.io',
+    assetsPublicPath: './',
     proxyTable: {},
 
     // Various Dev Server settings
